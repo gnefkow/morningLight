c7f4d0a67ec321a5584c5a5641f8cd8faebcc2d2
Kyle Becker <gnefkow87@gmail.com>
2019-11-30 20:32:05 -0500

    Pulls Sunrise and other times for the user's coordinates and converts them to the proper time zone and displays on the page.

---
+++ morningLight.js
@@ -0,0 +1,79 @@
+$( document ).ready(function() {
+
+var positionMessage = document.getElementById("positionMessage");
+
+
+var lat;
+var lng;
+
+// Find Tomorrow
+  moment.updateLocale('en', {
+    calendar : {
+        lastDay : '[Yesterday]',
+        sameDay : '[Today]',
+        nextDay : 'YYYY-MM-DD',
+        lastWeek : '[Last] dddd',
+        nextWeek : '[Next] dddd',
+        sameElse : 'L'
+    }
+  });
+  var tomorrow = moment().add(1, 'days').calendar(); 
+
+
+// Asks the user for their location:
+  function getLatLng() {
+    if(navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(onPositionUpdate);
+    } else {
+      alert("navigator.geolocation is not available");
+    }
+  }
+
+
+// Sets the latitude and Longitude for the location of the user's browser:
+  function onPositionUpdate(position) {
+      lat = position.coords.latitude;
+      lng = position.coords.longitude;
+      fetchSunrise();
+  }
+
+
+// Get the sunrise times
+  function fetchSunrise(){
+    
+    var queryURL = `https://api.sunrise-sunset.org/json?lat=${lat}&lng=${lng}&date=${tomorrow}&formatted=0`;
+        
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+      }).then(function(response) {
+        console.log(response);
+        var initialSunrise = response.results.sunrise;
+        var initialAstroTwilight = response.results.astronomical_twilight_begin;
+        var initialNauticalTwilight = response.results.nautical_twilight_begin;
+        var initialCivilTwilight = response.results.civil_twilight_begin;
+
+        var myTimezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+
+        var sunriseTimeUgly = moment(initialSunrise).tz(myTimezone);
+        
+
+        var sunriseTime = moment(sunriseTimeUgly).format("h:mm");
+
+        positionMessage.textContent= `Tomorrow the sun will rise at ${sunriseTime}`;
+
+        
+
+      });
+      
+      
+
+
+  }
+
+  
+
+getLatLng();
+
+ 
+}); // DOCUMENT READY
